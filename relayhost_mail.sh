9f217e725a0a5325c89b86a95c473a4debc1089f
mcsrainbow <guosuiyu@gmail.com>
2018-02-08 19:38:42 +0800

    Add relayhost_mail.sh

--- relayhost_mail.sh
+++ relayhost_mail.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 relayhost_item=$(grep relayhost /etc/postfix/main.cf || echo "No such relayhost")
-echo "${relayhost_item}" | mail -s "Relay Host Testing Mail From $HOSTNAME" $1
+echo -e "${relayhost_item}\nFrom $HOSTNAME" | mail -s "Relay Host Testing Mail From $HOSTNAME" $1
